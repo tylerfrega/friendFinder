97ee3d6d49f962396820d218b53b28a0ada4bc14
Tyler J. Frega <tyfrega@gmail.com>
2017-08-20 22:27:17 -0400

    progress

---
+++ app/data/friends.js
@@ -0,0 +1,60 @@
+var friendsList = [
+
+{
+     name:"Ahmed",
+     photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+     scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+
+  {
+  name:"John",
+  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores:[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+},
+
+{
+
+name:"Tim",
+     photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+     scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+
+
+
+];
+
+
+module.exports = friendsList;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,15 +5,17 @@ var path = require("path");
 
 module.exports = function(app) {
 
-     app.get("/survey", function(req, res) {
-    
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
       });
 
-    app.get("*", function(req, res) {
-
+    app.get("/home", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
-      });
+    });
+
+    // app.get("*", function(req, res) {
+    //     res.sendFile(path.join(__dirname, "../public/home.html"));
+    //   });
 
    
 
