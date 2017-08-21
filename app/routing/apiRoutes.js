1d5108408afbae54d0fc47df1114b91815ece461
Tyler J. Frega <tyfrega@gmail.com>
2017-08-20 23:00:44 -0400

    can now post data to api/friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,11 @@ module.exports = function(app){
 
     app.get("/api/friends", function(req, res){
         res.json(friends);
-        
     });
+
+    app.post("/api/friends", function(req, res){
+        friends.push(req.body);
+        res.json(true);
+    })
 
 };
