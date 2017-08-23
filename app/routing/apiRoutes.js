46326778daee1fdb2bcc1d7706180c4fa394c6bf
Tyler J. Frega <tyfrega@gmail.com>
2017-08-22 21:17:34 -0400

    able to compare survey results to existin friends and save scores

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,4 @@
+
 var friends = require('../data/friends');
 
 
@@ -8,8 +9,43 @@ module.exports = function(app){
     });
 
     app.post("/api/friends", function(req, res){
-        friends.push(req.body);
+       
+    var currentFriend = req.body;
+    var totalDifference = [];
+    var bestMatch;
+   
+     
+     for(i=0; i< friends.length; i++){
+        var differencesArray = [];
+
+         for(j=0; j < friends[i].scores.length; j++){
+            
+            var differences = Math.abs(friends[i].scores[j] - currentFriend.scores[j]);
+            differencesArray.push(differences);
+
+             }
+             sumArray(differencesArray, totalDifference);
+         }
+         
+         console.log(totalDifference);
+       
         res.json(true);
-    })
+    });
+
+   
+
+    function sumArray(arr, resultHolder ) {
+        var total = 0;
+        arr.forEach(function(element){
+            total += element;
+        })
+        resultHolder.push(total);
+    }
+
+
+
 
 };
+
+
+
