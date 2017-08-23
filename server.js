e959eefcb6553ba722f46ddf2b3fcd7f589603dc
Tyler J. Frega <tyfrega@gmail.com>
2017-08-23 13:06:17 -0400

    changes port for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require("path");
 var htmlRouter = require('./app/routing/htmlRoutes');
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
